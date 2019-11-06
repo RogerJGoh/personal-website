5b5ded3f50c16c43589ad0e02b373ffaf16d9796
Roger <rogergoh@outlook.my>
2019-11-06 08:24:46 +0800

    placeholder file

---
+++ app_server/models/placeholder.js
@@ -0,0 +1,8 @@
+//Considering the data used is simply a singular value of a String, 
+//I didn't think it was necessary to declare any complex data structures.
+
+//The models directory does however contain an authentication file for connecting to BigQuery,
+//However, is ignored in git to prevent leaking of credentials.
+
+//I've written this as a means of discussion as well as to commit the models directory to demonstrate MVC design.
+//Potentially will be used for more complex models in the future.
